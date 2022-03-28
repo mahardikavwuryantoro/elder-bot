ff0b33572fb98a19882451ac3ee8a39652a3bc00
MobyDick <mdikavw@gmail.com>
2022-03-29 00:30:13 +0700

    Added split embeds feature on urban

--- commands/urban.js
+++ commands/urban.js
@@ -29,16 +29,37 @@ module.exports = {
 
                const embed = new MessageEmbed()
                     .setTitle(word)
-                    .setURL(`https://www.urbandictionary.com/define.php?term=${words.join('%20')}`)
-                    .setDescription('-------------')
-                    .setFooter({text: 'Powered by Urban Dictionary'})
-                    .setColor('WHITE')
-               results.forEach(def => {
-                    embed.addField(wordHeader, def.definition)
-                    embed.addField('\u200b', '\u200b')
-
-               }) 
-               interaction.reply({embeds: [embed]})              
+               if(results.length > 5){
+                    const embed2 = new MessageEmbed()
+                    for(let i = 0; i <= results.length / 2 - 1; i++){
+                         embed.setURL(`https://www.urbandictionary.com/define.php?term=${words.join('%20')}`)
+                         embed.setDescription('-------------')
+                         embed.setColor('WHITE')
+                         embed.addField(`${i + 1}`, '\u200b', true)
+                         embed.addField(wordHeader, `${results[i].definition}`, true)
+                         embed.addField('\u200b', '\u200b')
+                    }
+                    for(let i = results.length / 2 ; i < results.length ; i++){
+                         embed2.setColor('WHITE')
+                         embed2.setFooter({text: 'Powered by Urban Dictionary'})
+                         embed2.addField(`${i + 1}`, '\u200b', true)
+                         embed2.addField(wordHeader, `${results[i].definition}`, true)
+                         embed2.addField('\u200b', '\u200b')
+                    }
+                    interaction.reply({embeds: [embed, embed2]})
+     
+               }else{
+                    results.forEach((def, i) => {
+                         embed.setURL(`https://www.urbandictionary.com/define.php?term=${words.join('%20')}`)
+                         embed.setDescription('-------------')
+                         embed.setColor('WHITE')
+                         embed.setFooter({text: 'Powered by Urban Dictionary'})
+                         embed.addField(`${i + 1}`, '\u200b', true)
+                         embed.addField(wordHeader, def.definition, true)
+                         embed.addField('\u200b', '\u200b')
+                    })
+                    interaction.reply({embeds: [embed]})
+               }              
           })
 
      },
@@ -48,24 +69,50 @@ module.exports = {
           ud.define(word, (err, results) => {
                if(err){
                    return msg.reply('Word unavailable')
-                   
                }
                const syllableRegex = /[^aeiouy]*[aeiouy]+(?:[^aeiouy]*$|[^aeiouy](?=[^aeiouy]))?/gi
                const wordHeader = args.map(word => word.match(syllableRegex)).map(word => word.join('.')).join(' - ')
 
                const embed = new MessageEmbed()
                     .setTitle(word)
-                    .setURL(`https://www.urbandictionary.com/define.php?term=${args.join('%20')}`)
-                    .setDescription('-------------')
-                    .setFooter({text: 'Powered by Urban Dictionary'})
-                    .setColor('WHITE')
-               results.forEach(def => {
-                    embed.addField(wordHeader, def.definition)
-                    embed.addField('\u200b', '\u200b')
+                    
+               
+               if(results.length > 5){
+                    const embed2 = new MessageEmbed()
+                    for(let i = 0; i <= results.length / 2 - 1; i++){
+                         embed.setURL(`https://www.urbandictionary.com/define.php?term=${args.join('%20')}`)
+                         embed.setDescription('-------------')
+                         embed.setColor('WHITE')
+                         embed.addField(`${i + 1}`, '\u200b', true)
+                         embed.addField(wordHeader, `${results[i].definition}`, true)
+                         embed.addField('\u200b', '\u200b')
+                    }
+                    for(let i = results.length / 2 ; i < results.length ; i++){
+                         embed2.setColor('WHITE')
+                         embed2.setFooter({text: 'Powered by Urban Dictionary'})
+                         embed2.addField(`${i + 1}`, '\u200b', true)
+                         embed2.addField(wordHeader, `${results[i].definition}`, true)
+                         embed2.addField('\u200b', '\u200b')
+                    }
+                    msg.reply({embeds: [embed, embed2]})
 
-               })
-               console.log(embed)
-               msg.reply({embeds: [embed]})              
+               }else{
+                    results.forEach((def, i) => {
+                         embed.setURL(`https://www.urbandictionary.com/define.php?term=${args.join('%20')}`)
+                         embed.setDescription('-------------')
+                         embed.setColor('WHITE')
+                         embed.setFooter({text: 'Powered by Urban Dictionary'})
+                         embed.addField(`${i + 1}`, '\u200b', true)
+                         embed.addField(wordHeader, def.definition, true)
+                         embed.addField('\u200b', '\u200b')
+                    })
+                    msg.reply({embeds: [embed]})
+               }
+               
+              
+                
+               
+             
           })
      }
 }
